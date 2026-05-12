7b59179b7b2521358bd94e4b20d5fa890c62b033
Aastha <aastha0808jain@gmail.com>
2026-05-12 09:48:49 +0530

    fix: silence

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,4 @@
-import * as THREE from "three";
 
-// Silence NaN bounding sphere warning
-const _computeBoundingSphere = THREE.BufferGeometry.prototype.computeBoundingSphere;
-THREE.BufferGeometry.prototype.computeBoundingSphere = function() {
-  if (this.attributes.position?.array.some(isNaN)) return;
-  _computeBoundingSphere.call(this);
-};
 
 import React from "react";
 import ReactDOM from "react-dom/client";
